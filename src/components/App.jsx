d75f26ddabb5732f6bb9587e9d53a11387ffd041
AShvab <shvabovska@gmail.com>
2023-05-01 00:42:58 +0300

    add task2,3,4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,11 @@
 import { Profile } from './Profile/Profile';
 import user from '../data/user.json';
+import { Statistics } from './Statistics/Statistics';
+import statistics from '../data/statistics.json';
+import {FriendList} from './FriendList/FriendList';
+import friends from '../data/friends.json';
+import {TransactionHistory} from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
@@ -11,7 +17,10 @@ export const App = () => {
     avatar={user.avatar}
     stats={user.stats}
   />
-    </div>
+<Statistics title="Upload stats" stats={statistics} />
+<FriendList friends={friends} />
+<TransactionHistory items={transactions} />
+    </div>  
   );
 };
 
