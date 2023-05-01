fe119edc1bd992ae98280e9377e4ce989451a328
AShvab <shvabovska@gmail.com>
2023-05-01 12:09:18 +0300

    fixed FriendListItem

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,27 +1,27 @@
-import PropTypes from 'prop-types';
 import css from './FriendList.module.css';
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+import PropTypes from 'prop-types';
 
 export const FriendList = ({ friends }) => {
   return (
-    <ul className={css.friends}>
-        {friends.map(({ id, avatar, name, isOnline }) => (
-      <li className={css.item} key={id}>
-        <span className={isOnline ? css.isOnline : css.isOffline}></span>
-        <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-        <p className={css.name}>{name}</p>
-      </li>))}
+    <ul className={css.friendList}>
+          {friends.map(({ id, avatar, name, isOnline }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
     </ul>
   );
 };
 
 FriendList.propTypes = {
-    friends: PropTypes.arrayOf(
-      PropTypes.exact({
-        id: PropTypes.number.isRequired,
-        avatar: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        isOnline: PropTypes.bool.isRequired,
-      })
-    ),
-  };
-  
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
+
